6d2c5bbcc7d7b6aa8c1ad8819a1fd972dbdd0dc5
Yohanes Andre Setiawan <andre44s.main@gmail.com>
2021-08-20 00:27:02 +0800

    Added to Github

--- SLAVE_MOTOR/SLAVE_MOTOR.ino
+++ SLAVE_MOTOR/SLAVE_MOTOR.ino
@@ -1,261 +1,103 @@
-//Parsing
+//Importing all the necesarry library
+#include "HardwareTimer.h"
+
+//Set PWM frequencies to 20khz
+#define freqPWM 20000
+#define freqCPU (72000000/freqPWM)
+#define maxOriginalPWM 65535
+#define amountOfMotor 4
+#define MAPfreq1 labs((pwmValue[0]/maxOriginalPWM) * (freqCPU))
+#define MAPfreq2 labs((pwmValue[1]/maxOriginalPWM) * (freqCPU))
+#define MAPfreq3 labs((pwmValue[2]/maxOriginalPWM) * (freqCPU))
+#define MAPfreq4 labs((pwmValue[3]/maxOriginalPWM) * (freqCPU))
+HardwareTimer pwmtimer(amountOfMotor);
+
+//Parsing data variable
 String dataIn;
-String arrayData[10];
-boolean parsing = false;
-boolean receive = false;
+String arrayData[8];
+boolean parsing = false,
+        receive = false,
+        readMasterState = true;
 
-//Motor 1
-const int mpwm1 = PB1; // Pin PWM
-const int mtr1a = PA8; // Pin Motor A
-const int mtr1b = PA9; // Pin Motor B
-#define ENC_COUNT_REV1 540 //Jumlah Gigi Encoder
-const int enc1a = PB14; //Pin Encoder
-const int enc1b = PB15; //Pin Encoder
-long revolutions1 = 0; // Putaran dalam satuan waktu
-long setRPM1 = 0; //Setpoint RPM
-long currentRPM1 = 0; //Kecepatan yang dibaca
+//Motor variable
+const uint8_t pwmPin[amountOfMotor]  = {PA0, PA1, PA2, PA3};
+const uint8_t enaAPin[amountOfMotor] = {PA4, PA6, PB0, PB10};
+const uint8_t enaBPin[amountOfMotor] = {PA5, PA7, PB1, PB11};
+const uint8_t encAPin[amountOfMotor] = {PB8, PB7, PB14, PB12};
+const uint8_t encBPin[amountOfMotor] = {PB9, PB6, PB15, PB13};
+const short int encCount[amountOfMotor] = {1988, 1988, 1988, 1988};
+float revolutions[amountOfMotor],
+      setRPM[amountOfMotor],
+      nowRPM[amountOfMotor];
 
-//Motor 2
-const int mpwm2 = PB0; // Pin PWM
-const int mtr2a = PA10; // Pin Motor A
-const int mtr2b = PC11; // Pin Motor B
-#define ENC_COUNT_REV2 540 //Jumlah Gigi Encoder
-const int enc2a = PB12; //Pin Encoder
-const int enc2b = PB13; //Pin Encoder
-long revolutions2 = 0; // Putaran dalam satuan waktu
-long setRPM2 = 0; //Setpoint RPM
-long currentRPM2 = 0; //Kecepatan yang dibaca
+//Relay variable
+const uint8_t amountOfRelay = 4;
+const uint8_t relayPin[amountOfRelay] = {PA15, PB3, PB4, PB5};
+uint8_t relayState[amountOfRelay];
 
-//Motor 3
-const int mpwm3 = PA7; // Pin PWM
-const int mtr3a = PB3; // Pin Motor 1
-const int mtr3b = PA15; // Pin Motor 2
-#define ENC_COUNT_REV3 540 //Jumlah Gigi Encoder
-const int enc3a = PB6; //Pin Encoder
-const int enc3b = PB7; //Pin Encoder
-long revolutions3 = 0; // Putaran dalam satuan waktu
-long setRPM3 = 0; //Setpoint RPM
-long currentRPM3 = 0; //Kecepatan yang dibaca
+//Timing Variable
+unsigned int prevTime, nowTime;
+const uint8_t interval = 100;
 
-//Motor 4
-const int mpwm4 = PA6; // Pin PWM
-const int mtr4a = PB4; // Pin Motor A
-const int mtr4b = PB5; // Pin Motor B
-#define ENC_COUNT_REV4 540 //Jumlah Gigi Encoder
-const int enc4a = PB8; //Pin Encoder
-const int enc4b = PB9; //Pin Encoder
-long revolutions4 = 0; // Putaran dalam satuan waktu
-long setRPM4 = 0; //Setpoint RPM
-long currentRPM4 = 0; //Kecepatan yang dibaca
+//PID variable
 
-long previousMillis = 0; // Waktu Sebelumnya
-long currentMillis = 0; // Waktu Sekarang
-unsigned long interval = 100; //Satuan Waktu untuk revolution1
+double PIDValue[amountOfMotor], PWMValue[amountOfMotor],
+       Error[amountOfMotor], PrevError[amountOfMotor],
+       P[amountOfMotor], I[amountOfMotor], D[amountOfMotor];
+double Kp[amountOfMotor] = {0, 0, 0, 0};
+double Ki[amountOfMotor] = {0, 0, 0, 0};
+double Kd[amountOfMotor] = {0, 0, 0, 0};
 
-//PID
-float PIDValue1 = 0, pwmValue1 = 0;
-double error1, previouserror1;
-double P_1 = 0, I_1 = 0, D_1 = 0;
-
-float PIDValue2 = 0, pwmValue2 = 0;
-double error2, previouserror2;
-double P_2 = 0, I_2 = 0, D_2 = 0;
-
-float PIDValue3 = 0, pwmValue3 = 0;
-double error3, previouserror3;
-double P_3 = 0, I_3 = 0, D_3 = 0;
-
-float PIDValue4 = 0, pwmValue4 = 0;
-double error4, previouserror4;
-double P_4 = 0, I_4 = 0, D_4 = 0;
-
-float Kp1 = 25;    //Kp MOTOR 1
-float Ki1 = 0;    //Ki MOTOR 1
-float Kd1 = 30;      //Kd MOTOR 1
-
-float Kp2 = 17;    //Kp MOTOR 2
-float Ki2 = 0;    //Ki MOTOR 2
-float Kd2 = 20;      //Kd MOTOR 2
-
-float Kp3 = 15;    //Kp MOTOR 3
-float Ki3 = 0;    //Ki MOTOR 3
-float Kd3 = 20;      //Kd MOTOR 3
-
-float Kp4 = 15;    //Kp MOTOR 3
-float Ki4 = 0;    //Ki MOTOR 3
-float Kd4 = 20;      //Kd MOTOR 3
+uint8_t i;
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(115200);
+  Serial1.begin(115200);
 
-  pinMode(enc1a, INPUT);
-  pinMode(enc1b, INPUT);
-  pinMode(mpwm1, PWM);
-  pinMode(mtr1a, OUTPUT);
-  pinMode(mtr1b, OUTPUT);
+  for (i = 0; i < amountOfMotor; i++) {
+    pinMode(pwmPin[i], PWM);
+    pinMode(enaAPin[i], OUTPUT);
+    pinMode(enaBPin[i], OUTPUT);
+    pinMode(encAPin[i], INPUT);
+    pinMode(encBPin[i], INPUT);
+  }
 
-  pinMode(enc2a, INPUT);
-  pinMode(enc2b, INPUT);
-  pinMode(mpwm2, PWM);
-  pinMode(mtr2a, OUTPUT);
-  pinMode(mtr2b, OUTPUT);
+  for (i = 0; i < amountOfRelay; i++) {
+    pinMode(relayPin[i], OUTPUT);
+  }
 
-  pinMode(enc3a, INPUT);
-  pinMode(enc3b, INPUT);
-  pinMode(mpwm3, PWM);
-  pinMode(mtr3a, OUTPUT);
-  pinMode(mtr3b, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(encAPin[0]), func1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encAPin[1]), func2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encAPin[2]), func3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encAPin[3]), func4, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encBPin[0]), func1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encBPin[1]), func2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encBPin[2]), func3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encBPin[3]), func4, CHANGE);
 
-  pinMode(enc4a, INPUT);
-  pinMode(enc4b, INPUT);
-  pinMode(mpwm4, PWM);
-  pinMode(mtr4a, OUTPUT);
-  pinMode(mtr4b, OUTPUT);
-
-  previousMillis = millis();
-  attachInterrupt(digitalPinToInterrupt(enc1a), func1, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc1b), func1, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc2a), func2, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc2b), func2, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc3a), func3, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc3b), func3, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc4a), func4, CHANGE); //Attach interrupt
-  attachInterrupt(digitalPinToInterrupt(enc4b), func4, CHANGE); //Attach interrupt
+  prevTime = millis();
 }
 
 void loop() {
-  cekData();
-  cekDataSerial();
+  readSerial();
+  readMaster();
 
-  currentMillis = millis();
-  if (currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;
+  nowTime = millis();
+  if (nowTime - prevTime > interval) {
+    prevTime = nowTime;
 
     // Calculate RPM
-    currentRPM1 = (float)(revolutions1 * 600 / ENC_COUNT_REV1);
-    currentRPM2 = (float)(revolutions2 * 600 / ENC_COUNT_REV2);
-    currentRPM3 = (float)(revolutions3 * 600 / ENC_COUNT_REV3);
-    currentRPM4 = (float)(revolutions4 * 600 / ENC_COUNT_REV4);
+    for (i = 0; i < amountOfMotor; i++) {
+      nowRPM[i] = (float)(revolutions[i] * 600 / encCount[i]);
+    }
 
-    Serial.print(setRPM1);
-    Serial.print("\t");
-    Serial.print(currentRPM1);
-    Serial.print("\t");
-    Serial.print(setRPM2);
-    Serial.print("\t");
-    Serial.print(currentRPM2);
-    Serial.print("\t");
-    Serial.print(setRPM3);
-    Serial.print("\t");
-    Serial.print(currentRPM3);
-    Serial.print("\t");
-    Serial.print(setRPM4);
-    Serial.print("\t");
-    Serial.println(currentRPM4);
-    Serial.flush();
-
-    VPID1();
-    VPID2();
-    VPID3();
-    VPID4();
-
-    revolutions1 = 0;
-    revolutions2 = 0;
-    revolutions3 = 0;
-    revolutions4 = 0;
-
-  }
-  runMotor();
-}
-
-void runMotor() {
-  pwmWrite(mpwm1, pwmValue1);
-  pwmWrite(mpwm2, pwmValue2);
-  pwmWrite(mpwm3, pwmValue3);
-  pwmWrite(mpwm4, pwmValue4);
-
-  if (setRPM1 > 0) {
-    //CounterClockWise
-    digitalWrite(mtr1a, HIGH);
-    digitalWrite(mtr1b, LOW);
-  }
-
-  if (setRPM1 < 0) {
-    //ClockWise
-    digitalWrite(mtr1a, LOW);
-    digitalWrite(mtr1b, HIGH);
-  }
-
-  if (setRPM1 == 0) {
-    //Stop
-    pwmWrite(mpwm1, 0);
-    digitalWrite(mtr1a, HIGH);
-    digitalWrite(mtr1b, HIGH);
-  }
-
-  if (setRPM2 > 0) {
-    //CounterClockWise
-    digitalWrite(mtr2a, HIGH);
-    digitalWrite(mtr2b, LOW);
-  }
-
-  if (setRPM2 < 0) {
-    //ClockWise
-    digitalWrite(mtr2a, LOW);
-    digitalWrite(mtr2b, HIGH);
-  }
-
-  if (setRPM2 == 0) {
-    //Stop
-    pwmWrite(mpwm2, 0);
-    digitalWrite(mtr2a, HIGH);
-    digitalWrite(mtr2b, HIGH);
-  }
-
-  if (setRPM3 > 0) {
-    //CounterClockWise
-    digitalWrite(mtr3a, HIGH);
-    digitalWrite(mtr3b, LOW);
-  }
-
-  if (setRPM3 < 0) {
-    //ClockWise
-    digitalWrite(mtr3a, LOW);
-    digitalWrite(mtr3b, HIGH);
-  }
-
-  if (setRPM3 == 0) {
-    //Stop
-    pwmWrite(mpwm3, 0);
-    digitalWrite(mtr3a, HIGH);
-    digitalWrite(mtr3b, HIGH);
-  }
-
-
-  if (setRPM4 > 0) {
-    //CounterClockWise
-    digitalWrite(mtr4a, HIGH);
-    digitalWrite(mtr4b, LOW);
-  }
-
-  if (setRPM4 < 0) {
-    //ClockWise
-    digitalWrite(mtr4a, LOW);
-    digitalWrite(mtr4b, HIGH);
-  }
-
-  if (setRPM4 == 0) {
-    //Stop
-    pwmWrite(mpwm4, 0);
-    digitalWrite(mtr4a, HIGH);
-    digitalWrite(mtr4b, HIGH);
+    PIDMotor();
   }
 }
 
-void cekData() {
-  while (Serial2.available() > 0) {
-    char inChar = (char)Serial2.read();
+void readMaster() {
+  while (Serial1.available() > 0 && readMasterState == true) {
+    char inChar = (char)Serial1.read();
     if (inChar == '*') {
       dataIn = "";
       receive = true;
@@ -276,12 +118,16 @@ void cekData() {
   }
 }
 
-void cekDataSerial() {
+void readSerial() {
   while (Serial.available() > 0) {
     char inChar = (char)Serial.read();
     if (inChar == '*') {
       dataIn = "";
       receive = true;
+    }
+
+    else if (inChar == 'E') {
+      readMasterState = !readMasterState;
     }
 
     if (receive) {
@@ -300,7 +146,7 @@ void cekDataSerial() {
 }
 
 void parsingData() {
-  int j = 0;
+  short int j = 0;
   arrayData[j] = "";
 
   for (int i = 1; i < dataIn.length(); i++) {
@@ -309,143 +155,77 @@ void parsingData() {
       arrayData[j] = "";
     }
     else {
-      arrayData[j] = arrayData[j] + dataIn[i];
+      arrayData[j] += dataIn[i];
     }
   }
-  setRPM1 = arrayData[0].toFloat();
-  setRPM2 = arrayData[1].toFloat();
-  setRPM3 = arrayData[2].toFloat();
-  setRPM4 = arrayData[3].toFloat();
+
+  setRPM[0] = arrayData[0].toFloat();
+  setRPM[1] = arrayData[1].toFloat();
+  setRPM[2] = arrayData[2].toFloat();
+  setRPM[3] = arrayData[3].toFloat();
+
+  relayState[0]  = arrayData[4].toFloat();
+  relayState[1]  = arrayData[5].toFloat();
+  relayState[2]  = arrayData[6].toFloat();
+  relayState[3]  = arrayData[7].toFloat();
 }
 
 void func1() {
-  revolutions1++;
+  revolutions[0]++;;
 }
 
 void func2() {
-  revolutions2++;
+  revolutions[1]++;;
 }
 
 void func3() {
-  revolutions3++;
+  revolutions[2]++;;
 }
 
 void func4() {
-  revolutions4++;
+  revolutions[3]++;;
 }
 
-void VPID1() {
+void PIDMotor() {
+  for (i = 0; i < amountOfMotor; i++) {
+    Error[i] = abs(setRPM[i]) - nowRPM[i];
+    P[i] = Error[i];
+    I[i] = I[i] + Error[i];
+    D[i] = Error[i] - PrevError[i];
 
-  error1 = abs(setRPM1) - currentRPM1;
-  P_1 = error1;
-  I_1 = I_1 + error1;
-  D_1 = error1 - previouserror1;
+    if (I[i] >= 65535) {
+      I[i] = 65535;
+    }
+    else if (I[i] <= 0) {
+      I[i] = 0;
+    }
 
-  if (I_1 > 65535) {
-    I_1 = 65535;
-  }
-  if (I_1 < (0)) {
-    I_1 = (0);
-  }
+    PIDValue[i] = (Kp[i] * P[i]) + (Ki[i] * I[i]) + (Kd[i] * D[i]);
+    PrevError[i] = Error[i];
+    PWMValue[i] = PWMValue[i] + PIDValue[i];
 
-  PIDValue1 = (Kp1 * P_1) + (Ki1 * I_1) + (Kd1 * D_1);
-  previouserror1 = error1;
-  pwmValue1 = pwmValue1 + PIDValue1;
+    if (PWMValue[i] >= 65535) {
+      PWMValue[i] = 65535;
+    }
+    else if (PWMValue[i] <= 0) {
+      PWMValue[i] = 0;
+    }
 
-  if (pwmValue1 >= 65535) {
-    pwmValue1 = 65535;
-  }
-  if (pwmValue1 <= (0)) {
-    pwmValue1 = (0);
-  }
-  if (setRPM1 == 0) {
-    pwmValue1 = 0;
-  }
-}
+    pwmWrite(pwmPin[i], PWMValue[i]);
 
-void VPID2() {
+    if (setRPM[i] > 0) {
+      digitalWrite(enaAPin[i], HIGH);
+      digitalWrite(enaBPin[i], LOW);
+    }
+    else if (setRPM[i] < 0) {
+      digitalWrite(enaAPin[i], LOW);
+      digitalWrite(enaBPin[i], HIGH);
+    }
+    else {
+      digitalWrite(enaAPin[i], LOW);
+      digitalWrite(enaBPin[i], LOW);
+    }
 
-  error2 = abs(setRPM2) - currentRPM2;
-  P_2 = error2;
-  I_2 = I_2 + error2;
-  D_2 = error2 - previouserror2;
-
-  if (I_2 > 65535) {
-    I_2 = 65535;
-  }
-  if (I_2 < (0)) {
-    I_2 = (0);
-  }
-
-  PIDValue2 = (Kp2 * P_2) + (Ki2 * I_2) + (Kd2 * D_2);
-  previouserror2 = error2;
-  pwmValue2 = pwmValue2 + PIDValue2;
-
-  if (pwmValue2 >= 65535) {
-    pwmValue2 = 65535;
-  }
-  if (pwmValue2 <= (0)) {
-    pwmValue2 = (0);
-  }
-  if (setRPM2 == 0) {
-    pwmValue2 = 0;
-  }
-}
-
-void VPID3() {
-
-  error3 = abs(setRPM3) - currentRPM3;
-  P_3 = error3;
-  I_3 = I_3 + error3;
-  D_3 = error3 - previouserror3;
-
-  if (I_3 > 65535) {
-    I_3 = 65535;
-  }
-  if (I_3 < (0)) {
-    I_3 = (0);
-  }
-
-  PIDValue3 = (Kp3 * P_3) + (Ki3 * I_3) + (Kd3 * D_3);
-  previouserror3 = error3;
-  pwmValue3 = pwmValue3 + PIDValue3;
-
-  if (pwmValue3 >= 65535) {
-    pwmValue3 = 65535;
-  }
-  if (pwmValue3 <= (0)) {
-    pwmValue3 = (0);
-  }
-  if (setRPM3 == 0) {
-    pwmValue3 = 0;
-  }
-}
-
-void VPID4() {
-
-  error4 = abs(setRPM4) - currentRPM4;
-  P_4 = error4;
-  I_4 = I_4 + error4;
-  D_4 = error4 - previouserror4;
-
-  if (I_4 > 65535) {
-    I_4 = 65535;
-  }
-  if (I_4 < (0)) {
-    I_4 = (0);
-  }
-
-  PIDValue4 = (Kp4 * P_4) + (Ki4 * I_4) + (Kd4 * D_4);
-  previouserror4 = error4;
-  pwmValue4 = pwmValue4 + PIDValue4;
-
-  if (pwmValue4 >= 65535) {
-    pwmValue4 = 65535;
-  }
-  if (pwmValue4 <= (0)) {
-    pwmValue4 = (0);
-  }
-  if (setRPM4 == 0) {
-    pwmValue4 = 0;
+    revolutions[i] = 0;
   }
 }
---
+++ SLAVE_MPU6050/SLAVE_MPU6050.ino
@@ -0,0 +1,267 @@
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//CODINGAN INI TIDAK BISA DITAMBAHKAN DELAY ATAUPUN SAMPLING DATA, KARNA AKAN MENGANGGU PEMBACAAN FASTWIRE DARI I2C
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+/* =========================================================================
+   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the Arduino's
+   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
+   digital I/O pin 2.
+   ========================================================================= */
+
+/* =========================================================================
+   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
+   when using Serial.write(buf, len). The Teapot output uses this method.
+   The solution requires a modification to the Arduino USBAPI.h file, which
+   is fortunately simple, but annoying. This will be fixed in the next IDE
+   release. For more info, see these links:
+
+   http://arduino.cc/forum/index.php/topic,109987.0.html
+   http://code.google.com/p/arduino/issues/detail?id=958
+   ========================================================================= */
+
+
+
+// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+// quaternion components in a [w, x, y, z] format (not best for parsing
+// on a remote host such as Processing or something though)
+//#define OUTPUT_READABLE_QUATERNION
+
+// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+// (in degrees) calculated from the quaternions coming from the FIFO.
+// Note that Euler angles suffer from gimbal lock (for more info, see
+// http://en.wikipedia.org/wiki/Gimbal_lock)
+//#define OUTPUT_READABLE_EULER
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+// components with gravity removed. This acceleration reference frame is
+// not compensated for orientation, so +X is always +X according to the
+// sensor, just without the effects of gravity. If you want acceleration
+// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+//#define OUTPUT_READABLE_REALACCEL
+
+// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+// components with gravity removed and adjusted for the world frame of
+// reference (yaw is relative to initial orientation, since no magnetometer
+// is present in this case). Could be quite handy in some cases.
+//#define OUTPUT_READABLE_WORLDACCEL
+
+// uncomment "OUTPUT_TEAPOT" if you want output that matches the
+// format used for the InvenSense teapot demo
+// #define OUTPUT_TEAPOT
+
+String pluss;
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+unsigned int reading = 0;
+double total = 0;
+double average = 0;
+double angle = 0;
+
+#include <SoftwareSerial.h>
+SoftwareSerial ArduinoSlave(3, 4);
+
+unsigned int prevTime, nowTime;
+const uint8_t interval = 100;
+
+void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  // initialize serial communication
+  // (115200 chosen because it is required for Teapot Demo output, but it's
+  // really up to you depending on your project)
+  Serial.begin(115200);
+  //while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+  // the baud timing being too misaligned with processor ticks. You must use
+  // 38400 or slower in these cases, or use some kind of external separate
+  // crystal solution for the UART timer.
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  //while (Serial.available() && Serial.read()); // empty buffer again
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(43);
+  mpu.setYGyroOffset(-27);
+  mpu.setZGyroOffset(26);
+  mpu.setXAccelOffset(775);
+  mpu.setYAccelOffset(3085);
+  mpu.setZAccelOffset(2012);
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
+  ArduinoSlave.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  prevTime = millis();
+}
+
+void loop() {
+  Gymbal();
+
+  total += angle;
+  reading++;
+
+  nowTime = millis();
+  if (nowTime - prevTime > interval) {
+    prevTime = nowTime;
+    average = total / reading;
+    Serial.println(average, 15);
+    Serial.println(reading);
+    ArduinoSlave.print("*");
+    ArduinoSlave.print(average, 15);
+    ArduinoSlave.print("#");
+
+    blinkState = !blinkState;
+
+    total = 0;
+    reading = 0;
+  }
+  digitalWrite(LED_PIN, blinkState);
+}
+
+void Gymbal() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize) {
+    if (mpuInterrupt && fifoCount < packetSize) {
+      // try to get out of the infinite loop
+      fifoCount = mpu.getFIFOCount();
+    }
+  }
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+  if (fifoCount < packetSize) {
+    //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+    // This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+  }
+  // check for overflow (this should never happen unless our code is too inefficient)
+  else if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+
+    // read a packet from FIFO
+    while (fifoCount >= packetSize) { // Lets catch up to NOW, someone is using the dreaded delay()!
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+    }
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    //Serial.print("ypr\t");
+    //Serial.print(ypr[0] * 180/M_PI);
+    //Serial.print("\t");
+    //Serial.print(ypr[1] * 180/M_PI);
+    //Serial.print("\t");
+    //Serial.println(ypr[2] * 180/M_PI);
+    angle = ypr[0] * 180 / M_PI;
+#endif
+  }
+}
